c3512c26f12ea2e674cd51e91d42ef32c3d52309
Leonardoyusuke <you@example.comleoyuhirano@gmail.com>
2022-10-23 00:23:02 +0100

    meio encaminhado

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 let numeroCartas=0;
 let qntcartasdif = [];
-
-
+let carta
+let card
+let inicio = false;
+let jogadas=0;
 const gifs = [
     'bobrossparrot.gif',
     'explodyparrot.gif',
@@ -23,22 +25,56 @@ function pergunta(){ // pergunta cartas
         qntcartasdif.push(gifs[i]);
         qntcartasdif.push(gifs[i]);
     }
+    qntcartasdif.sort(comparador);
     console.log(qntcartasdif)
 
     const lista = document.querySelector(".cartas1");
     for(let i = 0; i < numeroCartas ; i++){
         lista.innerHTML +=`
-        <div class="face1">
-             <div class="frente carta">
-                <img src="./imagens/${qntcartasdif[i]}">
+        <div class="card" onclick="virarCarta(this)">
+            <div class="back-face face ${qntcartasdif[i]}" >
+            <img id="foto" src="./imagens/${qntcartasdif[i]}">
             </div>
-            <div class="traseira carta">
+            <div class="front-face face">
                 <img src="./imagens/back.png">
-            </div>
-         </div>`;
-    }
-
+            </div>    
+        </div>`;
+    } //front e back invertidos//
+    
 }
+
+qntcartasdif.sort(comparador);
+let checar = 0
+let checar2;
+let cartas1;
+let cartas2;
+let lista;
+
+function virarCarta(cards){   
+    cards.classList.add("virar");
+    checar = checar + 1
+    console.log(checar)
+
+    const checarCarta = document.querySelectorAll(".virarCarta[0]")
+    const checarCarta2 = document.querySelectorAll(".virarCarta[1]")
+
+    if(checarCarta = checarCarta2){
+        checar = 0}
+
+    else if (checar == 2){
+    setTimeout(desvirarCarta, 2000)
+    checar = 0}
+    
+}
+
+function desvirarCarta(){
+     cards = document.getElementsByClassName("virar");
+
+    for(let indice = 0; indice < cards.length; indice++){
+        cards[indice].classList.remove("virar");
+        cards[indice].classList.remove("virar");
+        
+    }}
 function comparador() { 
     return Math.random() - 0.5; 
 }
